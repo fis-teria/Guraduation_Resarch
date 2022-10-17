66584f72c687b666354b30b087e7184cc68b5bc2
fis-teria <fate7str1ke4al1ce2belg@gmail.com>
2022-10-17 15:40:38 +0900

    20221017

--- build/analize.r
+++ build/analize.r
@@ -1,7 +1,11 @@
-setwd("D:\\openCV_WorkSpace\\Ohno_cmake_Desktop\\build\\result00_CSV")
-#setwd("D:\\openCV_WorkSpace\\Ohno_cmake_Desktop\\build\\result01_CSV")
-#setwd("/home/user/Guraduation_Resarch/Guraduation_Resarch/build/result00_CSV")
-#setwd("/home/user/Guraduation_Resarch/Guraduation_Resarch/build/result01_CSV")
+num = "4"
+switch(num,
+"1" = setwd("D:\\openCV_WorkSpace\\Ohno_cmake_Desktop\\build\\result00_CSV"),
+"2" = setwd("D:\\openCV_WorkSpace\\Ohno_cmake_Desktop\\build\\result01_CSV"),
+"3" = setwd("/home/user/Guraduation_Resarch/Guraduation_Resarch/build/result00_CSV"),
+"4" = setwd("/home/user/Guraduation_Resarch/Guraduation_Resarch/build/result01_CSV"),
+stop("only use 1 2 3 4")
+)
 getwd()
 
 x.data = c() #  空のベクトルを用意する．
@@ -9,9 +13,15 @@ y.data = c()
 z.data = c()
 u.data = c()
 
-data_size = 670 #データの取得 if 00 -> 670  if 01 -> 488
+data_size <- switch(num, #データの取得 if 00 -> 670  if 01 -> 488
+"1" = 670, 
+"2" = 488, 
+"3" = 670, 
+"4" = 488,
+stop("error") 
+)
 
-for (i in 1:data_size) {
+for (i in 0:data_size) {
   if (i < 10) {
     txt_name <- paste0("00000", i, ".csv") #ここを自分の結果のファイルに合うように変えてね
   } else if (i < 100) {
@@ -29,27 +39,32 @@ for (i in 1:data_size) {
 }
 
 
-for (i in 1:data_size) {
+for (i in 0:data_size) {
   #類似度
   if (i < 10) {
     txt_name <- paste0("00000", i, ".csv") #ここを自分の結果のファイルに合うように変えてね
-    png_name <- paste0("D:\\graResult\\result00_CSV\\00000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-    #png_name <- paste0("D:\\graResult\\result01_CSV\\00000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-    #png_name <- paste0("/home/user/Guraduation_Resarch/gra/result00_CSV/00000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-    #png_name <- paste0("/home/user/Guraduation_Resarch/gra/result01_CSV/00000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
+    switch(num,
+    "1" = png_name <- paste0("D:\\graResult\\result00_CSV\\00000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    "2" = png_name <- paste0("D:\\graResult\\result01_CSV\\00000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    "3" = png_name <- paste0("/home/user/Guraduation_Resarch/Guraduation/build/gra/result00_CSV/00000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    "4" = png_name <- paste0("/home/user/Guraduation_Resarch/uraduation/build/gra/result01_CSV/00000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    )
   } else if (i < 100) {
     txt_name <- paste0("0000", i, ".csv") #ここを自分の結果のファイルに合うように変えてね
-    png_name <- paste0("D:\\graResult\\result00_CSV\\0000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-    #png_name <- paste0("D:\\graResult\\result01_CSV\\0000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-    #png_name <- paste0("/home/user/Guraduation_Resarch/gra/result00_CSV/0000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-    #png_name <- paste0("/home/user/Guraduation_Resarch/gra/result01_CSV/0000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-
+    switch(num,
+    "1" = png_name <- paste0("D:\\graResult\\result00_CSV\\0000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    "2" = png_name <- paste0("D:\\graResult\\result01_CSV\\0000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    "3" = png_name <- paste0("/home/user/Guraduation_Resarch/uraduation/build/gra/result00_CSV/0000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    "4" = png_name <- paste0("/home/user/Guraduation_Resarch/uraduation/build/gra/result01_CSV/0000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    )
   } else if (i < 1000) {
     txt_name <- paste0("000", i, ".csv") #ここを自分の結果のファイルに合うように変えてね
-    png_name <- paste0("D:\\graResult\\result00_CSV\\000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-    #png_name <- paste0("D:\\graResult\\result01_CSV\\000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-    #png_name <- paste0("/home/user/Guraduation_Resarch/gra/result00_CSV/000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
-    #png_name <- paste0("/home/user/Guraduation_Resarch/gra/result01_CSV/000", i, ".png") #ここを自分の結果のファイルに合うように変えてね
+    switch(num,
+    "1" = png_name <- paste0("D:\\graResult\\result00_CSV\\000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    "2" = png_name <- paste0("D:\\graResult\\result01_CSV\\000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    "3" = png_name <- paste0("/home/user/Guraduation_Resarch/uraduation/build/gra/result00_CSV/000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    "4" = png_name <- paste0("/home/user/Guraduation_Resarch/uraduation/build/gra/result01_CSV/000", i, ".png"), #ここを自分の結果のファイルに合うように変えてね
+    )
 
   }
   txt_data <- read.table(txt_name, fileEncoding = "UTF-8", sep = ',')
@@ -61,11 +76,11 @@ for (i in 1:data_size) {
   par(new = T)
   plot(txt_data[, 1], txt_data[, 3], type = "l", ann = F, xlim = range(x.data), ylim = range(y.data), col = 4) #  
   par(new = T)
-  plot(txt_data[, 1], txt_data[, 4], type = "l", xlim = range(x.data), ylim = range(y.data), main = txt_name, xlab = "被マッチング画像の番号", ylab = "類似度", col = 5) #  
+  plot(txt_data[, 1], txt_data[, 4], type = "l", xlim = range(x.data), ylim = range(y.data), main = txt_name, xlab = "被マッチング画像の番号", ylab = "類似度", col = 3) #  
   par(xpd = T)
-  legend(par()$usr[2] + 0.2, par()$usr[4], legend = c("グレイスケール", "Canny", "Canny-small"), pch = c(1, 2, 3), col = c("red", "blue", "cyan"))
+  legend(par()$usr[2] + 0.5, par()$usr[4], legend = c("グレイスケール", "Canny", "Canny-small"), pch = c(1, 2, 3), col = c("red", "blue", "green"))
   par(xpd = T)
-  legend(par()$usr[2] + 0.2, par()$usr[3], legend = c("最大値", which.max(txt_data[,2]), which.max(txt_data[,3]),which.max(txt_data[,4])), pch = c(0, 1, 2, 3), col = c(0, "red", "blue", "cyan"))
+  legend(par()$usr[2] + 0.5, par()$usr[3] + 0.4, legend = c("最大値", which.max(txt_data[,2]), which.max(txt_data[,3]),which.max(txt_data[,4])), pch = c(0, 1, 2, 3), col = c(0, "red", "blue", "green"))
   dev.off()
   #}  else {
   #points(txt_data[, 1], txt_data[, 2], type = "l", xlim = range(x.data), ylim = range(y.data)) #  
--- src/canny.cpp
+++ src/canny.cpp
@@ -6,6 +6,66 @@
 
 using namespace cv;
 
+std::string make_path(std::string dir, int dir_num, int var)
+{
+    std::string back;
+    if (var < 10)
+    {
+        back = dir + std::to_string(dir_num) + "_template/00000" + std::to_string(var) + ".jpg";
+        return back;
+    }
+    else if (var < 100)
+    {
+        back = dir + std::to_string(dir_num) + "_template/0000" + std::to_string(var) + ".jpg";
+        return back;
+    }
+    else if (1 < 1000)
+    {
+        back = dir + std::to_string(dir_num) + "_template/000" + std::to_string(var) + ".jpg";
+        return back;
+    }
+}
+
+std::string make_rpath(std::string dir, int dir_num, int var)
+{
+    std::string back;
+    if (var < 10)
+    {
+        back = dir + std::to_string(dir_num) + "_CSV/00000" + std::to_string(var) + ".csv";
+        return back;
+    }
+    else if (var < 100)
+    {
+        back = dir + std::to_string(dir_num) + "_CSV/0000" + std::to_string(var) + ".csv";
+        return back;
+    }
+    else if (1 < 1000)
+    {
+        back = dir + std::to_string(dir_num) + "_CSV/000" + std::to_string(var) + ".csv";
+        return back;
+    }
+}
+
+std::string make_cpath(std::string dir, int dir_num, int var)
+{
+    std::string back;
+    if (var < 10)
+    {
+        back = dir + std::to_string(dir_num) + "/00000" + std::to_string(var) + ".jpg";
+        return back;
+    }
+    else if (var < 100)
+    {
+        back = dir + std::to_string(dir_num) + "/0000" + std::to_string(var) + ".jpg";
+        return back;
+    }
+    else if (1 < 1000)
+    {
+        back = dir + std::to_string(dir_num) + "/000" + std::to_string(var) + ".jpg";
+        return back;
+    }
+}
+
 int main(int argc, char **argv)
 {
     /*
@@ -14,32 +74,34 @@ int main(int argc, char **argv)
       return 1;
     }
     */
-    cv::Mat iim, sim, oim;
-    std::string dir = argv[1];
+    Mat rim, sim, tim;
+    Mat c_rim, c_sim, c_tim;
+    Mat cs_rim, cs_sim, cs_tim;
+    std::string dir = "Test0";
     std::cout << dir << "\n";
     std::string path;
     std::string dpath;
-    int i= atoi(argv[2]);
-    if (i < 10)
-    {
-        path = dir + "/00000" + std::to_string(i) + ".jpg";
-    }
-    else if (i < 100)
-    {
-        path = dir + "/0000" + std::to_string(i) + ".jpg";
-    }
-    else if (1 < 1000)
-    {
-        path = dir + "/000" + std::to_string(i) + ".jpg";
-    }
-    std::cout << path << "\n";
-    iim = cv::imread(path, 0);
-    cv::GaussianBlur(iim, sim, cv::Size(3, 3), 1.0);
-    cv::resize(sim, oim, Size(), 0.1, 0.1);
-    cv::Canny(oim, oim, atoi(argv[3]), atoi(argv[4]));
+    int i = atoi(argv[1]);
+    int j = atoi(argv[2]);
+    // test00
+    path = make_path(dir, 0, i);
+    rim = imread(path, IMREAD_GRAYSCALE);
+    Canny(imread(make_cpath(dir, 0, i)), c_rim, 100, 200);
+    Canny(rim, cs_rim, 200, 400);
+
+    // test01
+    dpath = make_path(dir, 1, j);
+    sim = imread(dpath, IMREAD_GRAYSCALE);
+    Canny(imread(make_cpath(dir, 1, j)), c_sim, 100, 400);
+    Canny(sim, cs_sim, 500, 600);
+
     /*** 画像を表示 ***/
-    cv::imshow("original", sim);
-    cv::imshow("canny", oim);
+    cv::imshow("1", rim);
+    cv::imshow("2", sim);
+    cv::imshow("3", c_rim);
+    cv::imshow("4", c_sim);
+    cv::imshow("5", cs_rim);
+    cv::imshow("6", cs_sim);
     cv::waitKey(0);
     return 0;
 }
