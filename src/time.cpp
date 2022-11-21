51f1515445f0d4da00c30187661241389d44e8fc
fis-teria <fate7str1ke4al1ce2belg@gmail.com>
2022-11-21 15:12:44 +0900

    20221121

--- src/time.cpp
+++ src/time.cpp
@@ -9,6 +9,8 @@
 #include <vector>
 #include <omp.h>
 #include <time.h>
+#include <numeric>
+#include <algorithm>
 
 //#define rootGunDai
 #define MATCH00to01
@@ -151,175 +153,6 @@ void save(int m)
   std::ofstream savefile(SAVE.c_str());
   savefile << m << std::endl;
   savefile.close();
-}
-
-void templete_Canny(int execute_num)
-{
-  cv::Mat rim, sim, tim;
-  cv::Mat c_rim, c_sim, c_tim;
-  cv::Mat cs_rim, cs_sim, cs_tim;
-  /*
-  rim = base image
-  sim = normal template iamge
-  tim = result normal template matching
-  c_rim = Canny-converted before resize rim
-  c_sim = Canny-converted before resizesim
-  c_tim = result cim template matching
-  cs_rim = Canny-converted after resize rim
-  cs_sim = Canny-converted after resize sim
-  cs_tim = result cim template matching
-
-  */
-  int i, j; // seed num
-  int num_t, num_m;
-  int path_t, path_m;
-  int load_num = read_save();
-  std::cout << load_num << std::endl;
-  int average = 0;
-  int result_size = 0;
-  std::vector<result> results;
-  std::string dir;
-  std::string r_dir;
-  std::string tag;
-  std::cout << dir << "\n";
-  std::string path;
-  std::string dpath;
-  std::string result_path;
-  int c_tnum1, c_tnum2;
-  int cs_tnum1, cs_tnum2;
-  int c_mnum1, c_mnum2;
-  int cs_mnum1, cs_mnum2;
-
-  double min_t, max_t;
-  double min_c, max_c;
-  double min_cs, max_cs;
-  cv::Point p_min_t, p_max_t;
-  cv::Point p_min_c, p_max_c;
-  cv::Point p_min_cs, p_max_cs;
-
-  switch (execute_num)
-  {
-  case 10: // Match 00 to 01
-    num_t = 671;
-    num_m = 489;
-    path_t = 0;
-    path_m = 1;
-    c_tnum1 = 100;
-    c_tnum2 = 200;
-    cs_tnum1 = 200;
-    cs_tnum2 = 400;
-    c_mnum1 = 100;
-    c_mnum2 = 400;
-    cs_mnum1 = 500;
-    cs_mnum2 = 600;
-    dir = "Test0";
-    r_dir = "result0";
-    tag = ".jpg";
-
-    break;
-  case 11: // Match 01 to 00
-    num_t = 489;
-    num_m = 671;
-    path_t = 1;
-    path_m = 0;
-    c_tnum1 = 100;
-    c_tnum2 = 400;
-    cs_tnum1 = 500;
-    cs_tnum2 = 600;
-    c_mnum1 = 100;
-    c_mnum2 = 200;
-    cs_mnum1 = 200;
-    cs_mnum2 = 400;
-    dir = "Test0";
-    r_dir = "result0";
-    tag = ".jpg";
-
-    break;
-  default:
-    std::cout << "error! can't use this number!!!!" << std::endl;
-    std::cout << "prease use one of these number\nGunDai Root\n11 base 00 template 01\n12 base 01 template 00\n\nTsukuba Root\n21 base 1001_1030 template 0917_1349\n22 bas 1001_1030 template 1001_1435" << std::endl;
-    return;
-  }
-  for (int m = load_num; m < num_t; m++)
-  {
-
-    //テンプレート画像へのパス
-    /*srand((unsigned int)time(NULL));
-    i = rand() % num_t;
-    sleep(1);
-    */
-    path = make_path(dir, path_t, m, tag);
-    result_path = make_rpath(r_dir, path_t, m);
-    std::cout << path << std::endl;
-    std::cout << result_path << std::endl;
-    std::ofstream outputfile(result_path);
-
-    rim = cv::imread(path, cv::IMREAD_GRAYSCALE);
-    cv::Canny(cv::imread(make_cpath(dir, path_t, m, tag)), c_rim, c_tnum1, c_tnum2);
-
-    cv::Canny(rim, cs_rim, cs_tnum1, cs_tnum2);
-
-    for (int k = 1; k < num_m; k++)
-    {
-      clock_t begin = clock();
-      /*マッチング画像へのパス
-      srand((unsigned int)time(NULL));
-      j = rand() % 490;
-      */
-      dpath = make_path(dir, path_m, k, tag);
-      std::cout << "match" << path << " : " << dpath << std::endl;
-
-      sim = cv::imread(dpath, cv::IMREAD_GRAYSCALE);
-
-      cv::matchTemplate(rim, sim(cv::Range(7, 87), cv::Range(10, 91)), tim, cv::TM_CCOEFF_NORMED);
-      cv::minMaxLoc(tim, &min_t, &max_t, &p_min_t, &p_max_t);
-
-      cv::Canny(cv::imread(make_cpath(dir, path_m, k, tag)), c_sim, c_mnum1, c_mnum2);
-      cv::matchTemplate(c_rim, c_sim(cv::Range(c_sim.rows / 10, (9 * c_sim.rows) / 10), cv::Range(c_sim.cols / 10, (9 * c_sim.cols) / 10)), c_tim, cv::TM_CCOEFF_NORMED);
-      cv::minMaxLoc(c_tim, &min_c, &max_c, &p_min_c, &p_max_c);
-
-      cv::Canny(sim, cs_sim, cs_mnum1, cs_mnum2);
-      cv::matchTemplate(cs_rim, cs_sim(cv::Range(cs_sim.rows / 10, (9 * cs_sim.rows) / 10), cv::Range(cs_sim.cols / 10, (9 * cs_sim.cols) / 10)), cs_tim, cv::TM_CCOEFF_NORMED);
-      cv::minMaxLoc(cs_tim, &min_cs, &max_cs, &p_min_cs, &p_max_cs);
-
-      results.resize(result_size + 1);
-      results[result_size].max_t = max_t;
-      results[result_size].max_c = max_c;
-      results[result_size].max_cs = max_cs;
-      results[result_size].num = k;
-      results[result_size].x_t = p_max_t.x;
-      results[result_size].y_t = p_max_t.y;
-      results[result_size].x_c = p_max_c.x;
-      results[result_size].y_c = p_max_c.y;
-      result_size++;
-      // printf("max = %f at (%d,%d)\n", max, p_max.x, p_max.y);
-      outputfile << k << "," << max_t << "," << max_c << "," << max_cs << std::endl;
-      // sleep(1);
-      clock_t end = clock();
-      print_elapsed_time(begin, end);
-    }
-
-    outputfile.close();
-    //類似度を昇順にソート
-    std::sort(results.begin(), results.end(), [](const result &x, const result &y)
-              { return x.max_t < y.max_t; });
-
-    //最大の類似度を持つマッチング画像へのパス
-    dpath = make_path(dir, path_m, results[result_size - 1].num, tag);
-    std::cout << path << " : " << dpath << std::endl;
-    std::cout
-        << "all max = " << results[result_size - 1].max_t << "(x ,y ) = (" << results[result_size - 1].x_t << ", " << results[result_size - 1].y_t << ")" << std::endl;
-
-    save(m);
-    /*
-    cv::namedWindow("template");
-    cv::imshow("template", rim);
-    cv::namedWindow("result");
-    cv::imshow("result", imread(dpath, 0));
-    cv::waitKey(0);
-    */
-  }
-  save(0);
 }
 
 int LBP_filter[3][3] = {{64, 32, 16},
@@ -513,208 +346,6 @@ void cvt_HOG(const cv::Mat &src, cv::Mat &hog)
   hog = image.clone();
 }
 
-void templete_LBPandHOG(int execute_num)
-{
-  cv::Mat rim, sim, tim;
-  cv::Mat l_rim, l_sim, l_tim;
-  cv::Mat sl_rim, sl_sim, sl_tim;
-  cv::Mat h_rim, h_sim, h_tim;
-  cv::Mat sh_rim, sh_sim, sh_tim;
-  /*
-  rim = base image
-  sim = normal template iamge
-  tim = result normal template matching
-  l_rim = LBP  before resize rim
-  l_sim = LBP  before resize sim
-  l_tim = result cim template matching
-  sl_rim = LBP  after resize rim
-  sl_sim = LBP  after resize sim
-  sl_tim = result cim template matching
-  h_rim = HOG  before resize rim
-  h_sim = HOG  before resizesim
-  h_tim = result cim template matching
-  sh_rim = HOG  after resize rim
-  sh_sim = HOG  after resize sim
-  sh_tim = result cim template matching
-
-  */
-  int i, j; // seed num
-  int num_t, num_m;
-  int path_t, path_m;
-  int load_num = read_save();
-  std::cout << load_num << std::endl;
-  int average = 0;
-  int result_size = 0;
-  std::vector<resultLH> results;
-  std::string dir;
-  std::string r_dir;
-  std::string tag;
-  std::cout << dir << "\n";
-  std::string path;
-  std::string dpath;
-  std::string result_path;
-  int c_tnum1, c_tnum2;
-  int cs_tnum1, cs_tnum2;
-  int c_mnum1, c_mnum2;
-  int cs_mnum1, cs_mnum2;
-
-  double min_l, max_l;
-  double min_sl, max_sl;
-  double min_h, max_h;
-  double min_sh, max_sh;
-  cv::Point p_min_l, p_max_l;
-  cv::Point p_min_sl, p_max_sl;
-  cv::Point p_min_h, p_max_h;
-  cv::Point p_min_sh, p_max_sh;
-
-  switch (execute_num)
-  {
-  case 10: // Match 00 to 01
-    num_t = 671;
-    num_m = 489;
-    path_t = 0;
-    path_m = 1;
-    c_tnum1 = 100;
-    c_tnum2 = 200;
-    cs_tnum1 = 200;
-    cs_tnum2 = 400;
-    c_mnum1 = 100;
-    c_mnum2 = 400;
-    cs_mnum1 = 500;
-    cs_mnum2 = 600;
-    dir = "Test0";
-    r_dir = "result0";
-    tag = ".jpg";
-
-    break;
-  case 11: // Match 01 to 00
-    num_t = 489;
-    num_m = 671;
-    path_t = 1;
-    path_m = 0;
-    c_tnum1 = 100;
-    c_tnum2 = 400;
-    cs_tnum1 = 500;
-    cs_tnum2 = 600;
-    c_mnum1 = 100;
-    c_mnum2 = 200;
-    cs_mnum1 = 200;
-    cs_mnum2 = 400;
-    dir = "Test0";
-    r_dir = "result0";
-    tag = ".jpg";
-
-    break;
-  default:
-    std::cout << "error! can't use this number!!!!" << std::endl;
-    std::cout << "prease use one of these number\nGunDai Root\n11 base 00 template 01\n12 base 01 template 00\n\nTsukuba Root\n21 base 1001_1030 template 0917_1349\n22 bas 1001_1030 template 1001_1435" << std::endl;
-    return;
-  }
-
-  for (int m = load_num; m < num_t; m++)
-  {
-
-    //テンプレート画像へのパス
-    /*srand((unsigned int)time(NULL));
-    i = rand() % num_t;
-    sleep(1);
-    */
-    path = make_path(dir, path_t, m, tag);
-    result_path = make_rpathLH(r_dir, path_t, m);
-    std::cout << path << std::endl;
-    std::cout << result_path << std::endl;
-    std::ofstream outputfile(result_path);
-
-    rim = cv::imread(path, cv::IMREAD_GRAYSCALE);
-    // LBP
-    cvt_LBP(cv::imread(make_cpath(dir, path_t, m, tag), 0), l_rim);
-    // small LBP
-    cvt_LBP(rim, sl_rim);
-    // HOG
-    cvt_HOG(cv::imread(make_cpath(dir, path_t, m, tag), 0), h_rim);
-    // small HOG
-    cvt_HOG(rim, sh_rim);
-
-    for (int k = 0; k < num_m; k++)
-    {
-      clock_t begin = clock();
-      /*マッチング画像へのパス
-      srand((unsigned int)time(NULL));
-      j = rand() % 490;
-      */
-      dpath = make_path(dir, path_m, k, tag);
-      std::cout << "match" << path << " : " << dpath << std::endl;
-
-      sim = cv::imread(dpath, cv::IMREAD_GRAYSCALE);
-
-      // LBP
-      cvt_LBP(cv::imread(make_cpath(dir, path_m, k, tag)), l_sim);
-      if (l_sim.empty())
-      {
-        return;
-      }
-      cv::matchTemplate(l_rim, l_sim(cv::Range(70, 870), cv::Range(100, 910)), l_tim, cv::TM_CCOEFF_NORMED);
-      cv::minMaxLoc(l_tim, &min_l, &max_l, &p_min_l, &p_max_l);
-      // small LBP
-      cvt_LBP(sim, sl_sim);
-      if (sl_sim.empty())
-      {
-        return;
-      }
-      cv::matchTemplate(sl_rim, sl_sim(cv::Range(7, 87), cv::Range(10, 91)), sl_tim, cv::TM_CCOEFF_NORMED);
-      cv::minMaxLoc(sl_tim, &min_sl, &max_sl, &p_min_sl, &p_max_sl);
-      // HOG
-      cvt_HOG(cv::imread(make_cpath(dir, path_m, k, tag), 0), h_sim);
-      if (h_sim.empty())
-      {
-        return;
-      }
-      cv::matchTemplate(h_rim, h_sim(cv::Range(70, 870), cv::Range(100, 910)), h_tim, cv::TM_CCOEFF_NORMED);
-      cv::minMaxLoc(h_tim, &min_h, &max_h, &p_min_h, &p_max_h);
-      // small HOG
-      cvt_HOG(sim, sh_sim);
-      if (sh_sim.empty())
-      {
-        return;
-      }
-      cv::matchTemplate(sh_rim, sh_sim(cv::Range(7, 87), cv::Range(10, 91)), sh_tim, cv::TM_CCOEFF_NORMED);
-      cv::minMaxLoc(sh_tim, &min_sh, &max_sh, &p_min_sh, &p_max_sh);
-
-      results.resize(result_size + 1);
-      results[result_size].max_l = max_l;
-      results[result_size].max_sl = max_sl;
-      results[result_size].max_h = max_sl;
-      results[result_size].max_sh = max_sl;
-      results[result_size].num = k;
-      result_size++;
-      // printf("max = %f at (%d,%d)\n", max, p_max.x, p_max.y);
-      outputfile << k << "," << max_l << "," << max_sl << "," << max_h << "," << max_sh << std::endl;
-      // sleep(1);
-      clock_t end = clock();
-      print_elapsed_time(begin, end);
-    }
-
-    outputfile.close();
-    //類似度を昇順にソート
-    //最大の類似度を持つマッチング画像へのパス
-    /*
-    dpath = make_path(dir, path_m, results[result_size - 1].num);
-    std::cout << path << " : " << dpath << std::endl;
-    std::cout
-        << "all max = " << results[result_size - 1].max_t << "(x ,y ) = (" << results[result_size - 1].x_t << ", " << results[result_size - 1].y_t << ")" << std::endl;
-    */
-    save(m);
-    /*
-    cv::namedWindow("template");
-    cv::imshow("template", rim);
-    cv::namedWindow("result");
-    cv::imshow("result", imread(dpath, 0));
-    cv::waitKey(0);
-    */
-  }
-  save(0);
-}
-
 int main(int argc, char **argv)
 {
   cv::Mat rim, sim, tim;
@@ -765,6 +396,24 @@ int main(int argc, char **argv)
   cv::Point p_min_sl, p_max_sl;
   cv::Point p_min_h, p_max_h;
   cv::Point p_min_sh, p_max_sh;
+  double time_c, time_sl, time_h;
+  std::vector<double> sum_time_c, sum_time_sl, sum_time_h;
+
+  num_t = 1;
+  num_m = 489;
+  path_t = 0;
+  path_m = 1;
+  c_tnum1 = 100;
+  c_tnum2 = 200;
+  cs_tnum1 = 200;
+  cs_tnum2 = 400;
+  c_mnum1 = 100;
+  c_mnum2 = 400;
+  cs_mnum1 = 500;
+  cs_mnum2 = 600;
+  dir = "Test0";
+  r_dir = "result0";
+  tag = ".jpg";
 
   for (int m = 0; m < num_t; m++)
   {
@@ -775,14 +424,12 @@ int main(int argc, char **argv)
     sleep(1);
     */
     path = make_path(dir, path_t, m, tag);
-    result_path = make_rpathLH(r_dir, path_t, m);
     std::cout << path << std::endl;
-    std::cout << result_path << std::endl;
-    std::ofstream outputfile(result_path);
+    std::ofstream outputfile("time.csv");
 
     rim = cv::imread(path, cv::IMREAD_GRAYSCALE);
-    
-    //Canny
+
+    // Canny
     cv::Canny(cv::imread(make_cpath(dir, path_t, m, tag)), c_rim, c_tnum1, c_tnum2);
     // small LBP
     cvt_LBP(rim, sl_rim);
@@ -791,7 +438,8 @@ int main(int argc, char **argv)
 
     for (int k = 0; k < num_m; k++)
     {
-      clock_t begin = clock();
+      clock_t begin;
+      clock_t end;
       /*マッチング画像へのパス
       srand((unsigned int)time(NULL));
       j = rand() % 490;
@@ -802,35 +450,46 @@ int main(int argc, char **argv)
       sim = cv::imread(dpath, cv::IMREAD_GRAYSCALE);
 
       // Canny
+      sum_time_c.resize(k + 1);
+      begin = clock();
       cv::Canny(cv::imread(make_cpath(dir, path_m, k, tag)), c_sim, c_mnum1, c_mnum2);
       cv::matchTemplate(c_rim, c_sim(cv::Range(c_sim.rows / 10, (9 * c_sim.rows) / 10), cv::Range(c_sim.cols / 10, (9 * c_sim.cols) / 10)), c_tim, cv::TM_CCOEFF_NORMED);
       cv::minMaxLoc(c_tim, &min_c, &max_c, &p_min_c, &p_max_c);
+      end = clock();
+      time_c = (double)(end - begin) / CLOCKS_PER_SEC;
+      sum_time_c[k] = time_c;
 
       // small LBP
+      sum_time_sl.resize(k + 1);
+      begin = clock();
       cvt_LBP(sim, sl_sim);
-      if (sl_sim.empty())
-      {
-        return;
-      }
       cv::matchTemplate(sl_rim, sl_sim(cv::Range(7, 87), cv::Range(10, 91)), sl_tim, cv::TM_CCOEFF_NORMED);
       cv::minMaxLoc(sl_tim, &min_sl, &max_sl, &p_min_sl, &p_max_sl);
+      end = clock();
+      time_sl = (double)(end - begin) / CLOCKS_PER_SEC;
+      sum_time_sl[k] = time_sl;
 
       // HOG
+      sum_time_h.resize(k + 1);
+      begin = clock();
       cvt_HOG(cv::imread(make_cpath(dir, path_m, k, tag), 0), h_sim);
-      if (h_sim.empty())
-      {
-        return;
-      }
       cv::matchTemplate(h_rim, h_sim(cv::Range(70, 870), cv::Range(100, 910)), h_tim, cv::TM_CCOEFF_NORMED);
       cv::minMaxLoc(h_tim, &min_h, &max_h, &p_min_h, &p_max_h);
+      end = clock();
+      time_h = (double)(end - begin) / CLOCKS_PER_SEC;
+      sum_time_h[k] = time_h;
 
-      outputfile << k << "," << max_l << "," << max_sl << "," << max_h << "," << max_sh << std::endl;
+      outputfile << k << "," << time_c << "," << time_sl << "," << time_h << "," << std::endl;
       // sleep(1);
-      clock_t end = clock();
-      print_elapsed_time(begin, end);
     }
     outputfile.close();
   }
+  double c = std::accumulate(sum_time_c.begin(), sum_time_c.end(), 0.0);
+  double sl = std::accumulate(sum_time_sl.begin(), sum_time_sl.end(), 0.0);
+  double h = std::accumulate(sum_time_h.begin(), sum_time_h.end(), 0.0);
+  std::cout << "average : " << c / num_m << ", " << sl / num_m << ", " << h / num_m << std::endl;
+  std::cout << "max time : " << *std::max_element(sum_time_c.begin(), sum_time_c.end()) << ", " << *std::max_element(sum_time_sl.begin(), sum_time_sl.end()) << ", " << *std::max_element(sum_time_h.begin(), sum_time_h.end()) << std::endl;
+  std::cout << "min time : " << *std::min_element(sum_time_c.begin(), sum_time_c.end()) << ", " << *std::min_element(sum_time_sl.begin(), sum_time_sl.end()) << ", " << *std::min_element(sum_time_h.begin(), sum_time_h.end()) << std::endl;
 
   return 0;
 }
